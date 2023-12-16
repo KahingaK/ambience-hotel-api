12f59f026d976b70077bfc3ecb92665f38a89e04
IvanKahinga <ikahinga@gmail.com>
2023-12-17 02:17:18 +0300

    Configuring capistrano gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,9 +21,8 @@ set :deploy_to, "/home/deployy/#{fetch :application}"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache',
- 'tmp/sockets', 'vendor/bundle',
- '.bundle', 'public/system', 'public/uploads', 'storage'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'vendor', '.bundle', 'public/system', 'public/uploads', 'storage'
+
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
 
